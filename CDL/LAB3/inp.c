140cd07b075f5690b92f2945caff8507ada424e4
KushalaAravapalli <96609641+Kushala01@users.noreply.github.com>
2023-08-21 10:13:23 +0530

    Create inp.c

---
+++ CDL/LAB3/inp.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+x = 10;
+y = 5;
+
+if (x == y) {
+    result = x + y;
+} else {
+    result = x - y;
+}
+
+if (x > y && y <= x) {
+    status = 1;
+} else {
+    status = 0;
+}
+
+if (y < x || x != y) {
+    flag = 1;
+} else {
+    flag = 0;
+}
+
+}
