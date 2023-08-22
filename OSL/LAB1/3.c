90254381f6c581a62a2581d6e8d54e8a5f6f38b5
KushalaAravapalli <96609641+Kushala01@users.noreply.github.com>
2023-08-22 16:30:33 +0530

    Updated

--- OSL/LAB1/3.c
+++ OSL/LAB1/3.c
@@ -1,3 +1,5 @@
+/*Demonstrate the use of different conversion specifiers and resulting output to
+allow the items to be printed.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -6,10 +8,17 @@ int main()
 {
     int a = 20;
     float b = 54.258;
-    char c = 'P';
-    char str[] = "Praveen";
+    char c = 'K';
+    char str[] = "Kushala";
+
+    // Printing integer 'a' using %d, floating-point 'b' using %f, character 'c' using %c,
+    // string 'str' using %s, and 'a' in hexadecimal using %x
     printf("a = %d, b = %f, c = %c, str = %s, Hexadecimal for a = %x\n", a, b, c, str, a);
-    errno = EPERM;
+
+    // Demonstrating error printing using %m
+    errno = EPERM; // Setting an error number
     printf("Error Access Number : %m\n");
+
     return 0;
 }
+
