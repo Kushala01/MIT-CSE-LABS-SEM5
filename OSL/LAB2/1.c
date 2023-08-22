f5db446553fc07b8cfe73b303dc7d97344db58b1
KushalaAravapalli <96609641+Kushala01@users.noreply.github.com>
2023-08-22 16:48:19 +0530

    Updated

--- OSL/LAB2/1.c
+++ OSL/LAB2/1.c
@@ -1,3 +1,6 @@
+/*Write a C program to emulate the ls -l UNIX command that prints all files in a
+current directory and lists access privileges, etc. DO NOT simply exec ls -l from the
+program.*/
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/stat.h>
