17dee2f2d253afbd530a37ddbbd26596a5c6f40f
KushalaAravapalli <96609641+Kushala01@users.noreply.github.com>
2023-08-22 18:49:20 +0530

    Updated

--- CNL/LAB3/q2c.c
+++ CNL/LAB3/q2c.c
@@ -1,5 +1,5 @@
 /*Write a client program to send a manually crafted HTTP request packet to a Web Server
-and display all fields received in HTTP Response at client Side.*/
+and display all fields received in HTTP Response at client Side.*/ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
