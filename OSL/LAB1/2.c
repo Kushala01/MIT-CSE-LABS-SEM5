559c048f0b55d56350a5f2a21a29e1280c540b62
KushalaAravapalli <96609641+Kushala01@users.noreply.github.com>
2023-08-22 16:27:26 +0530

    Updated

--- OSL/LAB1/2.c
+++ OSL/LAB1/2.c
@@ -1,3 +1,5 @@
+/*Write a program to list the files given as arguments, stopping every 20 lines until a
+key is hit. (a simple version of more UNIX utility)*/
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/stat.h>
@@ -5,42 +7,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-void main(int argc,char* argv[])
-{
-	if(argc != 2)
-	{
-		printf("Insufficient Arguments\n");
-		exit(1);
-	}
-	int fd=open(argv[1],O_RDONLY);
-	if(fd==-1)
-	{
-		printf("File Not Found\n");
-		exit(1);
-	}
-	char c,temp[100];
-	int i=0,k=0;
-	while(read(fd,&c,1)>0)
-	{
-		if(c!='\n')
-		{
-			temp[i]=c;
-			i++;
-		}
-		else
-		{
-			k++;
-			temp[i]='\0';
-			i=0;
-			printf("Line:%d \t %s \n", k,temp);
-			if(k%20==0)
-			{
-				char key;
-				printf("\n\nPress Any Key to Continue : \t");
-                scanf("%c",&key);
-				printf("\n\n");
+void main(int argc, char* argv[]) {
+    // Check if the correct number of arguments is provided
+    if (argc != 2) {
+        printf("Insufficient Arguments\n");
+        exit(1); // Exit with an error code
+    }
+
+    // Open the file for reading
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        printf("File Not Found\n");
+        exit(1); // Exit with an error code
+    }
+
+    char c; // To store individual characters from the file
+    char temp[100]; // To temporarily store content of a line from the file
+    int i = 0; // Counter for current position in the 'temp' array
+    int k = 0; // Counter for current line number
+
+    // Loop to read characters from the file
+    while (read(fd, &c, 1) > 0) {
+        // If the character is not a newline, add it to 'temp'
+        if (c != '\n') {
+            temp[i] = c;
+            i++;
+        }
+        else {
+            k++; // Increment line counter
+            temp[i] = '\0'; // Terminate 'temp' string
+            i = 0; // Reset 'i' for next line
+
+            // Print the line number and the line content
+            printf("Line:%d \t %s \n", k, temp);
+
+            // Check if 20 lines have been displayed
+            if (k % 20 == 0) {
+                char key;
+                printf("\n\nPress Any Key to Continue : \t");
+                scanf("%c", &key);
+                printf("\n\n");
             }
-		}
-	}
-	exit(0);
+        }
+    }
+
+    // Close the file
+    close(fd);
+
+    exit(0); // Exit with a success code
 }
