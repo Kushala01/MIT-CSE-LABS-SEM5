44f559b8cca7f43ad0c1cd781db26e4ca9f5cad8
KushalaAravapalli <96609641+Kushala01@users.noreply.github.com>
2023-08-24 10:26:40 +0530

    Updated

--- CNL/LAB1/q1c.c
+++ CNL/LAB1/q1c.c
@@ -1,42 +1,50 @@
+/*To illustrate encryption and decryption of messages using TCP. The client accepts
+messages to be encrypted through standard input device. The client will encrypt the string
+by adding 4(random value) to ASCII value of each alphabet. The encrypted message is
+sent to the server. The server then decrypts the message and displays both encrypted and
+decrypted forms of the string. Program terminates after one session.*/
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include<arpa/inet.h>
-#include<stdlib.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
 
-void main()
-{
-    int len,result,sock_id,n=1;
+void main() {
+    int len, result, sock_id, n = 1;
     struct sockaddr_in address;
-    char ch[256],buf[256];
+    char ch[256], buf[256];
 
-    sock_id = socket(AF_INET,SOCK_STREAM,0);
+    // Create a socket
+    sock_id = socket(AF_INET, SOCK_STREAM, 0);
 
-    address.sin_family=AF_INET;
-    address.sin_port=htons(6996);
-    address.sin_addr.s_addr=inet_addr("172.16.59.34");
-    len=sizeof(address);
+    // Set up the server address
+    address.sin_family = AF_INET;
+    address.sin_port = htons(6996);
+    address.sin_addr.s_addr = inet_addr("172.16.59.34");
+    len = sizeof(address);
 
-    result=connect(sock_id,&address,len);
+    // Connect to the server
+    result = connect(sock_id, &address, len);
 
-    if(result==-1)
-    {
+    if (result == -1) {
         perror("\nClient Error !!!!\n");
         exit(1);
     }
 
     printf("\nEnter String : \t");
     gets(ch);
-    ch[strlen(ch)]='\0';
+    ch[strlen(ch)] = '\0'; // Ensure string is null-terminated
 
-    for(int i=0;i<strlen(ch);i++)
-    {
+    // Encrypt the string by adding 4 to ASCII values of each character
+    for (int i = 0; i < strlen(ch); i++) {
         ch[i] += 4;
     }
 
-    write(sock_id,ch,strlen(ch));
+    // Send the encrypted string to the server
+    write(sock_id, ch, strlen(ch));
 
+    // Display the response from the server (note: missing read operation)
     puts(buf);
 }
