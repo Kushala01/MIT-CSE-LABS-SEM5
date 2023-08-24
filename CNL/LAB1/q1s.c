0e76b679922b3561c9c5378b9a04b1154b7e8147
KushalaAravapalli <96609641+Kushala01@users.noreply.github.com>
2023-08-24 10:28:43 +0530

    Updated

--- CNL/LAB1/q1s.c
+++ CNL/LAB1/q1s.c
@@ -1,47 +1,64 @@
+/*To illustrate encryption and decryption of messages using TCP. The client accepts
+messages to be encrypted through standard input device. The client will encrypt the string
+by adding 4(random value) to ASCII value of each alphabet. The encrypted message is
+sent to the server. The server then decrypts the message and displays both encrypted and
+decrypted forms of the string. Program terminates after one session.*/
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include<arpa/inet.h>
-#include<stdlib.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
 
 #define PORTNO 5656
 
-void main()
-{
-    int sock_id,new_sockid,portno,client_len,n=1;
-    struct sockaddr_in server_addr,client_addr;
-    int i,value;
+void main() {
+    int sock_id, new_sockid, portno, client_len, n = 1;
+    struct sockaddr_in server_addr, client_addr;
     
-    sock_id = socket(AF_INET,SOCK_STREAM,0);
+    // Create a socket
+    sock_id = socket(AF_INET, SOCK_STREAM, 0);
 
-    server_addr.sin_family=AF_INET;
-    server_addr.sin_addr.s_addr=inet_addr("172.16.59.34");
-    server_addr.sin_port=htons(PORTNO);
+    // Set up server address details
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("172.16.59.34"); // Server IP address
+    server_addr.sin_port = htons(PORTNO); // Port number
 
-    bind(sock_id,&server_addr,sizeof(server_addr));
+    // Bind the socket to the specified IP address and port number
+    bind(sock_id, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
-    listen(sock_id,5);
+    // Listen for incoming client connections
+    listen(sock_id, 5); // Allow up to 5 pending connections in the queue
 
-    while(1)
-    {
-        char buf[256]={""};
+    while (1) {
+        char buf[256] = ""; // Initialize a buffer to store incoming message
+        
         printf("\nServer Waiting ...\n");
 
-        client_len=sizeof(client_len);
+        client_len = sizeof(client_len);
 
-        new_sockid=accept(sock_id,&client_addr,&client_len);
+        // Accept a new client connection
+        new_sockid = accept(sock_id, (struct sockaddr*)&client_addr, &client_len);
 
-        n=read(new_sockid,buf,sizeof(buf));
+        // Read the encrypted message from the client
+        n = read(new_sockid, buf, sizeof(buf));
+        
+        // Display the encrypted message received from the client
+        printf("Encrypted Message from Client: %s\n", buf);
 
-        printf("Encrypted Message from Client : %s \n",buf);
-
-        for(int i=0;i<strlen(buf);i++)
-        {
+        // Decrypt the received message by subtracting 4 from each character
+        for (int i = 0; i < strlen(buf); i++) {
             buf[i] -= 4;
         }
 
-        printf("Decrypted Message from Client : %s \n",buf);
-    } 
+        // Display the decrypted message
+        printf("Decrypted Message from Client: %s\n", buf);
+        
+        // Note: The server response is not sent back to the client in this code
+        
+        // Close the socket for the current client
+        close(new_sockid);
+    }
 }
