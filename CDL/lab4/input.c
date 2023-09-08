e72b5d95193d5e5a9d91e14bccc510ebea352619
KushalaAravapalli <96609641+Kushala01@users.noreply.github.com>
2023-09-08 19:32:30 +0530

    Updated

--- CDL/lab4/input.c
+++ CDL/lab4/input.c
@@ -1,17 +1,21 @@
+/ in c
 #include <stdio.h>
-
+//hello 
+int c;
 int add(int a, int b) {
     return a + b;
 }
 
-int main() {
+void main() {
     int num1, num2;
+    #include<stdio.h>
 
     printf("Enter the first number: ");
     scanf("%d", &num1);
 
     printf("Enter the second number: ");
     scanf("%d", &num2);
+    scanf("#include<stdio.h>")
     int sum = add(num1, num2);
 
     printf("Sum: %d\n", sum);
