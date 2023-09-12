a3e25587593cd5d7730e15adddc5af16242f67d7
KushalaAravapalli <96609641+Kushala01@users.noreply.github.com>
2023-09-12 16:24:05 +0530

    Updated

--- OSL/LAB8/2wint.c
+++ OSL/LAB8/2wint.c
@@ -1,23 +1,43 @@
 /*Write a multithreaded program that calculates the summation of non-negative
 integers in a separate thread and passes the result to the main thread.*/
 
-#include <stdio.h>
-#include <pthread.h>
-#include <stdint.h> 
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<stdint.h>
 
-// Function to calculate the summation of non-negative integers
-void* calculateSum(void* arg) {
-    int n = *((int*)arg);
-    int sum = 0;
-
-    for (int i = 1; i <= n; i++) {
-        sum += i;
-    }
-
-    pthread_exit((void*)(intptr_t)sum);
+void* calculateSum(void * arg){
+	int n=*((int *)arg);
+	int sum=0;
+	for(int i=0;i<n;i++){
+		sum+=i;
+	}
+	pthread_exit((void *)(intptr_t)sum);
 }
 
-int main() {
+int main(char argc,char * argv[]){
+	if(argc!=2){
+		printf("u didnt enter the number: ");
+		return 1;
+	}
+	int n=atoi(argv[1]);
+	pthread_t tid;
+	int thread_arg=n;
+
+	if(pthread_create(&tid,0,calculateSum,&thread_arg)!=0){
+		perror("pthread_create");
+		return 1;
+	}
+	int result;
+
+	if(pthread_join(tid,(void **)&result)!=0){
+		perror("pthread_join");
+		return 1;
+	}
+	printf("\nthe ans is : %d \n",result);
+	return 0;
+}
+/*int main() {
     int n;
 
     // Prompt the user for input
@@ -27,25 +47,4 @@ int main() {
     if (n < 0 || n > 30) {
         printf("Please enter a non-negative integer up to 30.\n");
         return 1;
-    }
-
-    pthread_t tid;
-    int thread_arg = n;
-
-    // Create a thread to calculate the summation
-    if (pthread_create(&tid, 0, calculateSum, &thread_arg) != 0) {
-        perror("pthread_create");
-        return 1;
-    }
-
-    // Wait for the thread to finish and get the result
-    int result;
-    if (pthread_join(tid, (void**)&result) != 0) {
-        perror("pthread_join");
-        return 1;
-    }
-
-    printf("Sum of non-negative integers from 1 to %d: %d\n", n, result);
-
-    return 0;
-}
+    }*/
