0091b79238ffd0b7ef326fec30c56cb531046181
KushalaAravapalli <96609641+Kushala01@users.noreply.github.com>
2023-09-12 17:35:09 +0530

    Updated

--- OSL/LAB8/3WoStWoThCnt.c
+++ OSL/LAB8/3WoStWoThCnt.c
@@ -9,15 +9,11 @@ number to the given ending number.*/
 int start,end;
 
 int isPrime(int n){
-	if(n<=1)
-		return 0;
-	if(n==2)
-		return 1;
+	if(n<=1) return 0;
+	if(n==2) return 1;
 	for(int i=2;i<n;i++){
-		if(n%i==0)
-			return 0;
-		else
-			return 1;
+		if(n%i==0) return 0;
+		else return 1;
 	}
 }
 
