d30fb24de2cc25bfed43a5608263df5602fd0ec2
KushalaAravapalli <96609641+Kushala01@users.noreply.github.com>
2023-09-13 08:16:58 +0530

    Updated

--- OSL/LAB8/4.c
+++ OSL/LAB8/4.c
@@ -2,56 +2,66 @@
 numbers in an input array. Create a separate thread to perform the sum of even
 numbers and odd numbers. The parent thread has to wait until both the threads are
 done.*/
-#include <stdio.h>
-#include <pthread.h>
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdint.h>
 #define MAX_SIZE 100
 
-int numbers[MAX_SIZE];
-int size;
-int even_sum = 0;
-int odd_sum = 0;
+int arr[MAX_SIZE];
 
-void* compute_even_sum(void* param) {
-    for (int i = 0; i < size; i++) {
-        if (numbers[i] % 2 == 0) {
-            even_sum += numbers[i];
-        }
-    }
-    return 0;
+void * computeOddSum(void * size){
+	int n=*((int *)size);
+	int oddSum=0;
+	for(int i=0;i<n;i++){
+		if(i%2!=0){
+		oddSum+=arr[i];	
+		}
+	}
+	pthread_exit((void *)(intptr_t)oddSum);
+}
+void * computeEvenSum(void * size){
+	int n=*((int *)size);
+	int evenSum=0;
+	for(int i=0;i<n;i++){
+		if(i%2==0){
+		evenSum+=arr[i];	
+		}
+	}
+	pthread_exit((void *)(intptr_t)evenSum);
 }
 
-void* compute_odd_sum(void* param) {
-    for (int i = 0; i < size; i++) {
-        if (numbers[i] % 2 != 0) {
-            odd_sum += numbers[i];
-        }
-    }
-    return 0;
-}
+int main(){
+	int size,resultOdd,resultEven;
+	printf("enter the size of the array \n");
+	scanf("%d",&size);
 
-int main() {
-    printf("Enter the number of elements in the array (max %d): ", MAX_SIZE);
-    scanf("%d", &size);
+	printf("enter the elements of the array \n");
+	for(int i=0;i<size;i++){
+		scanf("%d",&arr[i]);
+	}
 
-    printf("Enter the array elements:\n");
-    for (int i = 0; i < size; i++) {
-        scanf("%d", &numbers[i]);
-    }
+	pthread_t odd,even;
 
-    pthread_t even_thread, odd_thread;
+	int thread_args=size;
 
-    // Create threads
-    pthread_create(&even_thread, 0, compute_even_sum, 0);
-    pthread_create(&odd_thread, 0, compute_odd_sum, 0);
-
-    // Wait for threads to finish
-    pthread_join(even_thread, 0);
-    pthread_join(odd_thread, 0);
-
-    // Print results
-    printf("Sum of even numbers: %d\n", even_sum);
-    printf("Sum of odd numbers: %d\n", odd_sum);
-
-    return 0;
+	if(pthread_create(&odd,0,computeOddSum,&thread_args)!=0){
+		perror("pthread_create");
+		return 1;
+	}
+	if(pthread_create(&even,0,computeEvenSum,&thread_args)!=0){
+		perror("pthread_create");
+		return 1;
+	}
+	if(pthread_join(odd,(void **)&resultOdd)!=0){
+		perror("pthread_join");
+		return 1;
+	}
+		if(pthread_join(even,(void **)&resultEven)!=0){
+		perror("pthread_join");
+		return 1;
+	}
+	printf("\nthe odd and even resulst are %d %d\n",resultOdd,resultEven);
+	return 0;
 }
