a7f6df0152caea478bf50fc030ac69df8124d1e2
KushalaAravapalli <96609641+Kushala01@users.noreply.github.com>
2023-09-19 20:29:32 +0530

    Updated

--- CNL/LAB2/q1s.c
+++ CNL/LAB2/q1s.c
@@ -8,95 +8,72 @@ client and sorts it and returns it to the client along with process id.*/
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <netinet/in.h>
 
 #define PORTNO 5656
 #define MAX_CLIENTS 5
 
-void sortArray(int arr[], int size) {
-    // Function to sort an integer array using bubble sort
-    int temp;
-    for (int i = 0; i < size; i++) {
-        for (int j = i + 1; j < size; j++) {
-            if (arr[i] > arr[j]) {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
+void sortArray(int arr[], int size){
+	int temp;
+	for(int i = 0; i < size; i++){
+		for(int j = i + 1; j < size; j++){
+			if(arr[i]>arr[j]){
+				temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
+		}
+	}
 }
 
-void handleClient(int client_socket) {
-    int arr_size, process_id;
-    int arr[100]; // Assuming a maximum of 100 integers in the array
-
-    // Receive array size and array data from the client
-    recv(client_socket, &arr_size, sizeof(arr_size), 0);
-    recv(client_socket, arr, sizeof(arr[0]) * arr_size, 0);
-
-    // Get the process ID
-    process_id = getpid();
-
-    // Sort the array
-    sortArray(arr, arr_size);
-
-    // Send the sorted array and process ID back to the client
-    send(client_socket, &process_id, sizeof(process_id), 0);
-    send(client_socket, arr, sizeof(arr[0]) * arr_size, 0);
-
-    close(client_socket);
+void handleClient(int client_socket ){
+	int arrSize, proccess_id;
+	int arr[100];
+	proccess_id=getpid();
+	//read(client_socket, &arrSize, sizeof(arrSize));
+	recv(client_socket, &arrSize, sizeof(arrSize), 0);
+	read(client_socket, arr, sizeof(arr[0])*arrSize);
+	sortArray(arr, arrSize);
+	write(client_socket, &proccess_id, sizeof(proccess_id));
+	write(client_socket, arr, sizeof(arr[0])*arrSize);
+	//printf("\n The proccess_id is: %d",proccess_id);
+	close(client_socket);
 }
 
-int main() {
-    int sock_id, client_sock;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len = sizeof(client_addr);
+void main(){
+	int sock_id, client_sock;
+	struct sockaddr_in server_addr, client_addr;
+	server_addr.sin_family=AF_INET;
+	server_addr.sin_port=htons(PORTNO);
+	server_addr.sin_addr.s_addr=INADDR_ANY;//accept any connections from any ip address
+	int client_len=sizeof(client_addr);
 
-    // Create a socket
-    sock_id = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock_id == -1) {
-        perror("Socket creation error");
-        exit(1);
-    }
+	sock_id=socket(AF_INET, SOCK_STREAM, 0);
 
-    // Set up server address details
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORTNO); // Port number
-    server_addr.sin_addr.s_addr = INADDR_ANY; // Accept connections from any IP address
+	if(bind(sock_id, (struct sockaddr* )&server_addr, sizeof(server_addr))==-1){
+		perror("Binding error");
+		exit(1);
+	}
 
-    // Bind the socket to the server address
-    if (bind(sock_id, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Binding error");
-        exit(1);
-    }
+	if(listen(sock_id, MAX_CLIENTS)==-1){
+		printf("listening error");
+		exit(1);
+	}
+	printf("Server is listening....\n");
+	while(1){
+		if((client_sock=accept(sock_id, (struct sockaddr *)&client_addr, &client_len))==-1){
+			printf("client_socket creation error");
+			exit(1);
+		}
+		printf("\nConnection accepted from client");
 
-    // Listen for incoming connections
-    if (listen(sock_id, MAX_CLIENTS) == -1) {
-        perror("Listening error");
-        exit(1);
-    }
+		if(fork()==0){
+			close(sock_id);
+			handleClient(client_sock);
+			exit(0);
+		}else{
+			close(client_sock);
+		}
 
-    printf("Server is listening...\n");
-
-    // Accept and handle client connections
-    while (1) {
-        // Accept a client connection
-        client_sock = accept(sock_id, (struct sockaddr *)&client_addr, &client_len);
-        if (client_sock == -1) {
-            perror("Accepting error");
-            exit(1);
-        }
-        printf("Connection accepted from a client\n");
-
-        // Create a child process to handle the client
-        if (fork() == 0) {
-            close(sock_id); // Child process doesn't need the listening socket
-            handleClient(client_sock); // Handle the client's request
-            exit(0); // Terminate the child process
-        } else {
-            close(client_sock); // Parent process doesn't need this client socket
-        }
-    }
-
-    return 0;
+	}
 }
