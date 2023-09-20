74b887f0e2b9e2413c4f9ce09a210c88a2b743db
KushalaAravapalli <96609641+Kushala01@users.noreply.github.com>
2023-09-20 20:04:49 +0530

    Updated

--- CNL/LAB3/q1s.c
+++ CNL/LAB3/q1s.c
@@ -1,56 +1,51 @@
 /*Write a UDP client-server program where t he client sends rows of a matrix, and the
 server combines them together as a matrix.*/ 
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include<time.h>
 
 #define PORTNO 5656
+#define MAX_CLIENTS 5
 
-int main() {
-    int sd;  // Socket descriptor
-    int m, n;  // Matrix dimensions
-    struct sockaddr_in sadd, cadd;  // Server and client address structures
-    
-    // Create a UDP socket
-    sd = socket(AF_INET, SOCK_DGRAM, 0);
-    sadd.sin_family = AF_INET;
-    sadd.sin_addr.s_addr = inet_addr("127.0.0.1");  // Server IP address
-    sadd.sin_port = htons(PORTNO);  // Port number
-    
-    // Bind the socket to the server's address
-    int result = bind(sd, (struct sockaddr *)&sadd, sizeof(sadd));
-    int len = sizeof(cadd);
-    
-    // Receive matrix dimensions from the client
-    recvfrom(sd, &m, sizeof(m), 0, (struct sockaddr *)&cadd, &len);
-    recvfrom(sd, &n, sizeof(n), 0, (struct sockaddr *)&cadd, &len);
-    
-    int arr[m][n];  // Array to hold the combined matrix
-    int recvarr[n];  // Temporary array to receive matrix rows
-    int rowCount = 0;  // Counter for rows
-    
-    // Loop to receive rows and build the combined matrix
-    while (rowCount < m) {
-        recvfrom(sd, recvarr, n * sizeof(int), 0, (struct sockaddr *)&cadd, &len);
-        for (int i = 0; i < n; i++) {
-            arr[rowCount][i] = recvarr[i];  // Store row data in the combined matrix
+void main(){
+    int sd, m ,n, len;
+    struct sockaddr_in server_addr, client_addr;
+
+    sd=socket(AF_INET, SOCK_DGRAM, 0);
+    server_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    server_addr.sin_family=AF_INET;
+    server_addr.sin_port=htons(PORTNO);
+
+    len=sizeof(client_addr);
+    bind(sd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+    recvfrom(sd, &m, sizeof(m), 0,(struct sockaddr *)&client_addr, &len);
+    recvfrom(sd, &n, sizeof(n), 0,(struct sockaddr *)&client_addr, &len);
+
+    int arr[m][n];
+    int revarr[n];
+    int rowCount=0;
+
+    while(rowCount<m){
+        recvfrom(sd, revarr, n*sizeof(int), 0, (struct sockaddr *)&client_addr, &len);
+        for(int i=0;i<n;i++){
+            arr[rowCount][i]=revarr[i];
         }
         rowCount++;
     }
-    
-    // Display the combined matrix
     printf("After combining the matrix is: \n");
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%d ", arr[i][j]);  // Print matrix element
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            printf("%d ",arr[i][j]);
         }
         printf("\n");
-    }
-    
-    return 0;  
+    }   
 }
