083bd60a53a292e963e89db6584875b87253f2c5
KushalaAravapalli <96609641+Kushala01@users.noreply.github.com>
2023-09-20 19:08:01 +0530

    Updated

--- CNL/LAB2/aq1s.c
+++ CNL/LAB2/aq1s.c
@@ -4,92 +4,51 @@ should also send the process id to the client.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <time.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include<time.h>
 
-#define PORTNO 10400
+#define PORTNO 5656
+#define MAX_CLIENTS 5
 
-// Function to handle client requests
-void handleClient(int newsockfd) {
-    char buf[256];
-    time_t now;
-    struct tm *timeinfo;
-    pid_t pid = getpid();
-    char pid_str[16];
-
-    // Get current time and convert it to a string
-    time(&now);
-    timeinfo = localtime(&now);
-    strftime(buf, sizeof(buf), "Server time: %Y-%m-%d %H:%M:%S", timeinfo);
-
-    // Convert the process ID to a string and append to the response
-    sprintf(pid_str, "PID: %d", pid);
-    strcat(buf, "\n");
-    strcat(buf, pid_str);
-
-    // Send the response to the client
-    write(newsockfd, buf, strlen(buf));
-    close(newsockfd);
-    exit(0); // Terminate the child process
+void handleClient(int newsockid){
+    printf("created a new child with pid : %d\n",getpid());
+    time_t t;
+    time(&t);
+    char tt[256];
+    strcpy(tt, ctime(&t));
+    int pid=getpid();
+    write(newsockid, &pid, sizeof(pid));
+    write(newsockid, tt, sizeof(tt));
+    close(newsockid);
+    //exit(0);
 }
+void main(){
+    int sockid, newsockid, clilen, n=1;
+    int size;
+    struct sockaddr_in client_addr, server_addr;
+    sockid=socket(AF_INET, SOCK_STREAM, 0);
+    server_addr.sin_family=AF_INET;
+    server_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    server_addr.sin_port=htons(PORTNO);
+    bind(sockid, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
-int main() {
-    int sockfd, newsockfd, clilen;
-    struct sockaddr_in seraddr, cliaddr;
+    listen(sockid, 5);
 
-    // Create a socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        perror("Socket creation error");
-        exit(1);
-    }
+   while(1){
+        clilen=sizeof(clilen);
+        newsockid=accept(sockid, (struct sockaddr *)&client_addr, &clilen);
 
-    // Set up server address details
-    memset(&seraddr, 0, sizeof(seraddr));
-    seraddr.sin_family = AF_INET;
-    seraddr.sin_addr.s_addr = INADDR_ANY;
-    seraddr.sin_port = htons(PORTNO);
-
-    // Bind the socket to the server address
-    if (bind(sockfd, (struct sockaddr *)&seraddr, sizeof(seraddr)) < 0) {
-        perror("Binding error");
-        exit(1);
-    }
-
-    // Listen for incoming connections
-    if (listen(sockfd, 5) < 0) {
-        perror("Listen error");
-        exit(1);
-    }
-
-    printf("Server is listening on port %d...\n", PORTNO);
-
-    // Accept and handle client connections
-    while (1) {
-        clilen = sizeof(cliaddr);
-        newsockfd = accept(sockfd, (struct sockaddr *)&cliaddr, &clilen);
-        if (newsockfd < 0) {
-            perror("Accept error");
-            exit(1);
-        }
-
-        // Create a child process to handle the client
-        pid_t child_pid = fork();
-        if (child_pid < 0) {
-            perror("Fork error");
-            exit(1);
-        } else if (child_pid == 0) {
-            // Child process handles client
-            close(sockfd); // Close listening socket in child
-            handleClient(newsockfd);
-        } else {
-            // Parent process continues listening
-            close(newsockfd); // Close new socket in parent
+        if(fork() == 0){
+            close(sockid);
+            handleClient(newsockid);
+            exit(0);
+        }else{
+            close(newsockid);
         }
     }
-
-    return 0;
 }
